d85f8532a9ce95ad3870b3266a7d1311c3b8c5d9
AI-3Tech <injalacharya1@gmail.com>
2024-09-06 17:48:03 +0545

    Website complete

---
+++ joke.js
@@ -0,0 +1,507 @@
+let jokes = [`Why can’t you hear a pterodactyl going to the bathroom? <br>
+
+=>  Because the “P” is silent.`,
+
+`&#8226; 3. What do you call a well-balanced horse? <br>
+
+=>  Stable.`,
+
+`&#8226; 4. What do you call an angry carrot? <br>
+
+=>  A steamed veggie.`,
+
+`&#8226; 5. Where do polar bears keep their money? <br>
+
+=>  In a snowbank.`,
+
+`&#8226; 6. How do you make an egg-roll? <br>
+
+=>  You push it!`,
+
+`&#8226; 7. What would bears be without bees? <br>
+
+=>  Ears.`,
+
+`&#8226; 8. What do you call a pile of cats? <br>
+
+=>  A meow-ntain.`,
+
+`&#8226; 9. Why do cows wear bells? <br>
+
+=>  Because their horns don’t work.`,
+
+`&#8226; Why did the bicycle fall over? <br>
+
+=>  Because it was two tired.`,
+
+`&#8226; What did the triangle say to the circle? <br>
+
+=>  You’re pointless.`,
+
+
+`&#8226; Why was Cinderella so bad at soccer? <br>
+
+=>  She kept running away from the ball!`,
+
+`&#8226; What do lawyers wear to court? <br>
+
+=>  Lawsuits.`,
+
+`&#8226; What do elves learn in school? <br>
+
+=>  The elf-abet.`,
+
+`&#8226; Where was King David’s temple located? <br>
+
+=>  Beside his ear.`,
+
+`&#8226; What did one toilet say to another? <br>
+
+=>  You look flushed.`,
+
+`&#8226; What lights up a soccer stadium? <br>
+
+=>  A soccer match.`,
+
+`&#8226; What does corn say when it gets a compliment? <br>
+
+=>  Aw, shucks!`,
+
+`&#8226; What’s the difference between a poorly dressed man on a tricycle and a well-dressed man on a bicycle? <br>
+
+=>  Attire.`,
+
+`&#8226; What’s red and bad for your teeth? <br>
+
+=>  A brick.`,
+
+`&#8226; What do sprinters eat before they race? <br>
+
+=>  Nothing. They fast.`,
+
+`&#8226; What has more lives than a cat? <br>
+
+=>  A frog, because it croaks every day.`,
+
+`&#8226; Why was the fish’s grades bad? <br>
+
+=>  They were below sea level.`,
+
+`&#8226; What’s Forrest Gump’s password? <br>
+
+=>  1forrest1.`,
+
+`&#8226; What do you call a fish without an eye? <br>
+
+=>  Fsh.`,
+
+`&#8226; Why shouldn't you use a broken pencil? <br>
+
+=>  Because it's pointless.`,
+
+`&#8226; What do you call a pig that practices karate? <br>
+
+=>  A pork chop.`,
+
+`&#8226; What has four wheels and flies? <br>
+
+=>  A garbage truck!`,
+
+`&#8226; What did the tomato say to the other tomato during a race? <br>
+
+=>  Ketchup.`,
+
+`&#8226; Why shouldn’t you write with a broken pen? <br>
+
+=>  Because it’s pointless.`,
+
+`&#8226; Why can't you trust the king of the jungle? <br>
+
+=>  Because he's always lion.`,
+
+`&#8226; What did one wall say to the other? <br>
+
+=>  "I'll meet you at the corner."`,
+
+`&#8226; What do sea monsters eat? <br>
+
+=>  Fish and ships.`,
+
+`&#8226; What do you call a sad strawberry? <br>
+
+=>  A blueberry!`,
+
+`&#8226; Why are pirates called pirates? <br>
+
+=>  They just ARRRR!`,
+
+`&#8226; How do you organize a space party? <br>
+
+=>  You planet.`,
+
+`&#8226; Why do seagulls fly over the sea? <br>
+
+=>  If they flew over the bay, they would be bagels.`,
+
+`&#8226; What do cows read the most? <br>
+
+=>  Cattle-logs.`,
+
+`&#8226; What do clouds wear under their shorts? <br>
+
+=>  Thunder pants!`,
+
+`&#8226; How many tickles does it take to make an octopus laugh? <br>
+
+=>  Ten-tickles.`,
+
+`&#8226; What did 0 say to 8? <br>
+
+=>  "Nice belt."`,
+
+`&#8226; How did the hipster burn his tongue? <br>
+
+=>  He drank his coffee before it was cool.`,
+
+`&#8226; What did the drummer name her twin daughters? <br>
+
+=>  Anna 1, Anna 2.`,
+
+`&#8226; What did the lettuce say to the celery? <br>
+
+=>  Quit stalking me!`,
+
+`&#8226; What’s small and red and has a rough voice? <br>
+
+=>  A hoarse raddish!`,
+
+`&#8226; Why do mushrooms get invited to all the parties? <br>
+
+=>  Because they are such fungis.`,
+
+`&#8226; Why shouldn’t you tell secrets in a cornfield? <br>
+
+=>  Too many ears.`,
+
+`&#8226; Why couldn’t the bad sailor learn the alphabet? <br>
+
+=>  Because he always got lost at “C.”`,
+
+`&#8226; What kind of cheese isn’t yours? <br>
+
+=>  Nacho cheese.`,
+
+`&#8226; What does a spy do when he is cold? <br>
+
+=>  He goes undercover.`,
+
+`&#8226; How does the moon cut his hair? <br>
+
+=>  Eclipse it.`,
+
+`&#8226; When do computers overheat? <br>
+
+=>  When they need to vent.`,
+
+`&#8226; Why did the scarecrow win an award? <br>
+
+=>  Because he was outstanding in his field.`,
+
+`&#8226; If athletes get athlete’s foot, what do elves get? <br>
+
+=>  Mistle-toes.`,
+
+`&#8226; What’s brown and sticky? <br>
+
+=>  A stick.`,
+
+`&#8226; What do you call a sad cup of coffee? <br>
+
+=>  Depresso.`,
+
+`&#8226; How did the telephone propose to his girlfriend? <br>
+
+=>  He gave her a ring.`,
+
+`&#8226; What did the cucumber say to the pickle? <br>
+
+=>  You mean a great dill to me.`,
+
+`&#8226; Why didn't the melons get married? <br>
+
+=>  Because they cantaloupe.`,
+
+`&#8226; Did you hear the rumor about butter? <br>
+
+=>  Well, I'm not going to go spreading it!`,
+
+`&#8226; What happens when a snowman throws a tantrum? <br>
+
+=>  He has a meltdown.`,
+
+`&#8226; What goes up and down but doesn’t move? <br>
+
+=>  Stairs.`,
+
+`&#8226; Why did the cookie go to the hospital? <br>
+
+=>  Because he felt crumby.`,
+
+`&#8226; What did the janitor say when he jumped out of the closet? <br>
+
+=>  Supplies!`,
+
+`&#8226; What do you get when you cross a fish and an elephant? <br>
+
+=>  Swimming trunks.`,
+
+`&#8226; What did the worker at the rubber band factory say when he was fired? <br>
+
+=>  Oh snap.`,
+
+`&#8226; Why can’t a nose be 12 inches long? <br>
+
+=>  Because then it would be a foot.`,
+
+`&#8226; Why did the belt go to jail? <br>
+
+=>  Because it held up a pair of pants.`,
+
+`&#8226; Where does the general put his armies? <br>
+
+=>  In his sleevies.`,
+
+`&#8226; What do you call a magician that looses his magic? <br>
+
+=>  Ian.`,
+
+`&#8226; Why did the golfer bring two pairs of pants? <br>
+
+=>  In case he got a hole in one.`,
+
+`&#8226; What did the yoga instructor say when her landlord tried to evict her? <br>
+
+=>  Namaste.`,
+
+`&#8226; How do rabbits travel? <br>
+
+=>  By hareplanes.`,
+
+`&#8226; What do you call a sleeping dinosaur? <br>
+
+=>  A dino-snore.`,
+
+`&#8226; Why did the kid stock up on yeast? <br>
+
+=>  He wanted to make some dough.`,
+
+`&#8226; What did the football coach say to the broken vending machine? <br>
+
+=>  Give me my quarterback.`,
+
+`&#8226; Why are elephants wrinkly? <br>
+
+=>  Because you can’t iron them.`,
+
+`&#8226; What did the cake say to the fork? <br>
+
+=>  You want a piece of me? <br>`,
+
+`&#8226; How did the two cats end their fight? <br>,
+
+=> They hissed and made up.`,
+
+`&#8226; Why did the strawberry cry? <br>
+
+=>  He found himself in a jam.`,
+
+`&#8226; What’s black and white and read all over? <br>
+
+=>  A newspaper!`,
+
+`&#8226; How did the barber win the race? <br>
+
+=>  He knew a shortcut!`,
+
+`&#8226; What did 50 Cent do when he was hungry? <br>
+
+=>  58!`,
+
+`&#8226; What did the nut say to the other nut in a game of tag? <br>
+
+=>  Imma cashew!`,
+
+`&#8226; Why did an old man fall in a well? <br>
+
+=>  Because he couldn’t see that well!`,
+
+`&#8226; Why is Peter Pan always flying? <br>
+
+=>  Because he Neverlands.`,
+
+`&#8226; Why don’t astronomers like Orion’s Belt? <br>
+
+=>  It’s a big waist of space.`,
+
+`&#8226; What do you call a hippie’s wife? <br>
+
+=>  Mississippi.`,
+
+`&#8226; Why are peppers the best at archery? <br>
+
+=>  Because they habanero!`,
+
+`&#8226; How do you tell the difference between a bull and a cow? <br>
+
+=>  It is either one or the udder!`,
+
+`&#8226; What did one plate say to the other? <br>
+
+=>  Dinner is on me!`,
+
+`&#8226; Why do hummingbirds hum? <br>
+
+=>  Because they don’t know the words!`,
+
+`&#8226; Why do cows have hooves and not feet? <br>
+
+=>  They lactose.`,
+
+`&#8226; What do you call a chicken that is staring at a lettuce? <br>
+
+=>  Chicken sees a salad.`,
+
+`&#8226; What do you call a bear with no teeth? <br>
+
+=>  A gummy bear!`,
+
+`&#8226; Which bear is the most condescending? <br>
+
+=>  A pan-duh!`,
+
+`&#8226; Why are spiders so smart? <br>
+
+=>  They can find everything on the web.`,
+
+`&#8226; What do you call a blind dinosaur? <br>
+
+=>  A do-you-think-he-saw-us.`,
+
+`&#8226; How do you make a water bed bouncier? <br>
+
+=>  Add spring water.`,
+
+`&#8226; What job did the frog have at the hotel? <br>
+
+=>  Bellhop.`,
+
+`&#8226; What kind of car does an egg drive? <br>
+
+=>  A yolkswagen.`,
+
+`&#8226; What kind of tea is hard to swallow? <br>
+
+=>  Reality!`,
+
+`&#8226; Why wouldn't the shrimp share his treasure? <br>
+
+=>  He was a little shellfish!`,
+
+`&#8226; Did you hear about the sensitive burglar? <br>
+
+=>  He takes things personally!`,
+
+`&#8226; When does a duck wake up? <br>
+
+=>  At the quack of dawn!`,
+
+`&#8226; What does a baby computer call his father? <br>
+
+=>  Data!`,
+
+`&#8226; What do you get from a pampered cow? <br>
+
+=>  Spoiled milk!`,
+
+`&#8226; Why was the bee's hair always sticky? <br>
+
+=>  He used a honeycomb.`,
+
+`&#8226; What do you call phoney spaghetti? <br>
+
+=>  An im-pasta.`,
+
+`&#8226; What do you call an alligator detective? <br>
+
+=>  An investi-gator.`,
+
+`&#8226; What vegetables are a sailor’s enemies? <br>
+
+=>  Leeks`,
+
+`&#8226; Why did the banana go to the doctor? <br>
+
+=>  Because it wasn’t peeling well.`,
+
+`&#8226; What’s a firefly’s favorite dance? <br>
+
+=>  The glitterbug.`,
+
+`&#8226; Where do roses sleep at night? <br>
+
+=>  In their flowerbed.`,
+
+`&#8226; Why was the politician out of breath? <br>
+
+=>  He was running for office.`,
+
+`&#8226; What did the left eye say to the right eye? <br>
+
+=>  Between you and me, something smells.`,
+
+`&#8226; Why aren’t koalas actual bears? <br>
+
+=>  They don’t meet koalafications.`,
+
+`&#8226; Why can’t you play hockey with pigs? <br>
+
+=>  They always hog the puck.`,
+
+`&#8226; What classical ballet did the pig pen perform? <br>
+
+=>  Swine Lake.`,
+
+`&#8226; What’s a ballerina’s favorite number? <br>
+
+=>  Two-two.`,
+
+`&#8226; How did the duck buy lipstick? <br>
+
+=>  She just put it on her bill.`,
+
+`&#8226; What kind of tree can fit inside your hand? <br>
+
+=>  A palm tree.`,
+
+`&#8226; What steals from you when you're in your bathtub? <br>
+
+=>  A robber duckie.`,
+
+`&#8226; What kind of dog tells time? <br>
+
+=>  A watch dog.`,
+
+`&#8226; How do you identify a bald eagle? <br>
+
+=>  All his feathers are combed over to one side.`,
+
+`&#8226; Why does a tiger have stripes? <br>
+
+=>  So he will not be spotted.`];
+
+
+
+function myFunction() {
+    let random = Math.floor(Math.random() * (jokes.length-1));
+    document.getElementById("joke").innerHTML = jokes[random];
+}
